139660c59bba414892b499088045eba2fc46640c
faradev93 <faramarz.khanlo@gmail.com>
2025-01-05 02:22:32 +0330

    ok shod

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import CardWrapper from "./components/CardWrapper";
+import "./Glob-Tail.css";
+import Flexy from "./components/Flex";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const monster = [];
+// async function testi() {
+//   const data2 = await fetch("https://reqres.in/api/users?page=2");
+//   const tabdil = await data2.json();
+//   console.log;
+// }
+// testi();
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Flexy>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => {
+          return <CardWrapper />;
+        })}
+      </Flexy>
+    </div>
+  );
 }
-
-export default App
---
+++ src/components/CardWrapper/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+export default function () {
+  return (
+    
+    <div className="arguman">
+      <a href="http://www.crafteo.ai" target="_blank">
+        Test
+      </a>
+    </div>
+  );
+}
